a6685816671e569434c43799059e8d5a144b29c9
ranto <rantoirwanda08@gmail.com>
2025-09-12 10:37:02 +0700

    fix: image ratio bug

--- src/components/core/section/section-content/ContentImage.tsx
+++ src/components/core/section/section-content/ContentImage.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
 const ContentImage = () => (
-  <div className="layout-main w-auto md:p-0 md:m-0 md:min-w-[380px] flex justify-center">
+  <div className="layout-main w-auto md:p-0 md:m-0 md:min-w-[384px] flex justify-center">
     <Image
       alt="topup"
       src="/images/contents/topup.webp"
       width={256}
-      height={259}
-      className="w-full max-w-[380px] h-auto"
+      height={256}
+      className="w-full max-w-[384px]"
     />
   </div>
 );
