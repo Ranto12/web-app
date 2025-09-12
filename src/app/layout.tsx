846e027da839f96f8dd08907e60bf9c4833dabce
ranto <rantoirwanda08@gmail.com>
2025-09-12 13:51:16 +0700

    fix: google speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Work_Sans } from "next/font/google";
 const workSans = Work_Sans({
   variable: "--font-work-sans",
   subsets: ["latin"],
+  display: 'swap'
 });
 
 export const metadata: Metadata = {
